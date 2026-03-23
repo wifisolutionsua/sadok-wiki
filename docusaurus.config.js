d2c3774efc86b9f0525615a3891cf8bbbc2a35d2
Serg <47433128+wifisolutionsua@users.noreply.github.com>
2026-03-23 16:56:21 +0200

    Comment out 'More' section in docusaurus.config.js
    Comment out the 'More' section in the Docusaurus config.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -132,19 +132,19 @@ const config = {
               },
             ],
           },
-          {
-            title: 'More',
-            items: [
-              {
-                label: 'Blog',
-                to: '/blog',
-              },
-              {
-                label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
-              },
-            ],
-          },
+          // {
+          //   title: 'More',
+          //   items: [
+          //     {
+          //       label: 'Blog',
+          //       to: '/blog',
+          //     },
+          //     {
+          //       label: 'GitHub',
+          //       href: 'https://github.com/facebook/docusaurus',
+          //     },
+          //   ],
+          // },
         ],
         copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
       },
