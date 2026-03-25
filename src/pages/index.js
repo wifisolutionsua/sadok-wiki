319db7d148ff532575a40f75f63e854712706212
serg <serg@wifisolutions.com.ua>
2026-03-25 14:04:09 +0200

    Remove welcome2 and add nested folder

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,43 +1,14 @@
-import clsx from 'clsx';
-import Link from '@docusaurus/Link';
+import {useEffect} from 'react';
+import {useHistory} from '@docusaurus/router';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
-
-import Heading from '@theme/Heading';
-import styles from './index.module.css';
-
-function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
-  return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <Heading as="h1" className="hero__title">
-          {siteConfig.title}
-        </Heading>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
-          </Link>
-        </div>
-      </div>
-    </header>
-  );
-}
 
 export default function Home() {
+  const history = useHistory();
   const {siteConfig} = useDocusaurusContext();
-  return (
-    <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
-      <HomepageHeader />
-      <main>
-        <HomepageFeatures />
-      </main>
-    </Layout>
-  );
+
+  useEffect(() => {
+    history.replace(`${siteConfig.baseUrl}docs/intro`);
+  }, [history, siteConfig.baseUrl]);
+
+  return null;
 }
