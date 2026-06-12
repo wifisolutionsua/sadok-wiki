eb1939411604dadb8f7a50f7ceb09e4ac9d8e5a7
Serg <47433128+wifisolutionsua@users.noreply.github.com>
2026-06-12 15:39:25 +0300

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -12,7 +12,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 const config = {
   title: 'Sadok - CRM для дитячих закладів',
   tagline: 'Wiki',
-  favicon: 'img/favicon.ico',
+  favicon: 'img/favicon.png',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
   future: {
