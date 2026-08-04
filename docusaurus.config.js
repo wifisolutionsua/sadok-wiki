e33583934bc50a1f3d070cf235653ecbc70ca6b2
Serg <47433128+wifisolutionsua@users.noreply.github.com>
2026-08-04 13:19:09 +0300

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -78,7 +78,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
+      image: 'img/docusaurus-social-card.png',
       colorMode: {
         respectPrefersColorScheme: true,
       },
